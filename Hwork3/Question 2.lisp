341d1ca2b6465b39b517da32b6719dd0e8e9ac9f
Justin-HAL <100646646+Justin-HAL@users.noreply.github.com>
2025-02-12 15:39:37 +0000

    finished question 2

--- Hwork3/Question 2.lisp
+++ Hwork3/Question 2.lisp
@@ -6,17 +6,25 @@
     (ldiff L (part-right L))
 )
 
+;maybe use a cond instead of if
+
+
 (defun merge-lists (L1 L2)
-    (defvar res ())
-    (if (and (< 0 (length L1)) (< 0 (length L2)))
-        ;if both lists have more than 0 elements.
-        (loop 
-            (if (< (car L1) (car (L2)))
-                (progn (setq res (append res (car L1))) (setq L1 (cdr L1)))
-                (progn (setq res (append res (car L2))) (setq L2 (cdr L2)))
-            )
-            (when)
-    
-    
-    )
-)
+  (let ((res ())) ; return varianle
+    (loop while (and L1 L2)
+          do (if (< (car L1) (car L2))
+          ; iF true
+                 (progn (setq res (append res (list (car L1)))) (setq L1 (cdr L1)))
+            ;if false
+                 (progn (setq res (append res (list (car L2)))) (setq L2 (cdr L2)))))
+    (if L1
+        (setq res (append res L1))
+        (setq res (append res L2)))
+    res))
+
+(defun merge-sort (L)
+  (if (or (null L) (null (cdr L)))
+      L
+      (let ((left (part-left L)) (right (part-right L)))
+        (merge-lists (merge-sort left) (merge-sort right))
+        )))
