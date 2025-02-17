f0557111ecaa4a8277dcaffb8b78a9f564ef804d
Justin-HAL <100646646+Justin-HAL@users.noreply.github.com>
2025-02-17 00:43:21 +0000

    fixed Q4 to make it not use loops

--- Hwork3/Question4.lisp
+++ Hwork3/Question4.lisp
@@ -2,29 +2,11 @@
 ;sorted is a sorted list.
 ;value is a symbol.
 (defun insert (sorted value)
-  (let ((less '()) ;empty list, contains elements that have been checked and arre confimed to be less than value.
-        (unchecked sorted) ; this list conatins the elements that have not been checked yet.
-        (res '())) ; this list will contain the sorted list with the value inserted.
-    (if (< (car (last unchecked)) value) ; if the value of the last element of the sorted list is less than the value we are comparing, just put value at the back of the list.
-        (setq res (append unchecked (list value)))
-
-      ;while there are more elements to check.
-      (loop while (> (length unchecked) 0)
-        do (if (<= value (car unchecked)) ; if the current value is less then the first unchecked element.
-              ;if true /////////////////////////////////////////
-              (progn
-                (setq less (append less (list value))) ;insert value at the back of the less list. since value is greater than all these elements, this is the correct sorted position
-                (setq res (append less unchecked)) ;add the rest of the elements to the list
-                (setq unchecked '())) ; Exit loop,
-              ;///////////////////////////////////////////////////
-              ; else ////////////////////////////////////////////
-              (progn
-                (setq less (append less (list (car unchecked)))) ; add first element of unchecked to checked, as that element has been compared.
-                (setq unchecked (cdr unchecked))))) ; remove the checked element from the unchecked list.
-              ;/////////////////////////////////////////////////////
-    )
-    res); return res
-  )
+  (cond
+    ((null sorted) (list value)) ; If the list is empty, return a new list with the value
+    ((<= value (car sorted)) (cons value sorted)) ; If the value is less than or equal to the first element, insert it at the beginning
+    (t (cons (car sorted) (insert (cdr sorted) value))))) ; Otherwise, recursively insert the value in the rest of the list
+  
  
  ;this function will be recusivly called to insert unsorted elements into the sorted list.
 (defun insertion-helper (sorted unsorted)
